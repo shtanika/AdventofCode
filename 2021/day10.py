da970007c2e9cec4a49f42fe9b45bcabf4fd3e34
shamaliat01 <carnationskies@gmail.com>
2021-12-11 23:49:16 -0500

    final

--- 2021/day10.py
+++ 2021/day10.py
@@ -1,4 +1,5 @@
 import numpy as np
+import statistics
 
 input = open("2021/inputs/10.txt")
 lines=[l.strip() for l in input]
@@ -23,18 +24,42 @@ def findIncorrect(line,points):
     idxs.append(line.find('>'))
 
     if(max(idxs)>0):
-        idxs = [i if i!=-1 else 99 for i in idxs]
+        idxs = [i if i!=-1 else max(idxs)+1 for i in idxs]
         points += vals[np.argmin(idxs)]
     return points
 
+def removeCorrupted(lines):
+    incomplete=[]
+    for line in lines:
+        if(line.find(')')==-1 and line.find(']')==-1 and line.find('}')==-1 and line.find('>')==-1):
+            incomplete.append(line)
+    return incomplete
+
+def getScore(line):
+    score=0
+    line = line[::-1]
+    for i in line:
+        if(i=='('):
+            score = score*5+1
+        elif(i=='['):
+            score = score*5+2
+        elif(i=='{'):
+            score = score*5+3
+        elif(i=='<'):
+            score = score*5+4
+    return score
 
 # PART ONE
-removePairs(lines)
-
+lines = removePairs(lines)
 points = 0
 for line in lines:
-    #print(f"{len(line)} {line}")
     points = findIncorrect(line,points)
 print(f"PART ONE\nTotal syntax error score: {points}\n")
 
 # PART TWO
+lines = removeCorrupted(lines)
+scores = []
+for line in lines:
+    score = getScore(line)
+    scores.append(score)
+print(f"PART TWO\nMiddle score: {statistics.median(scores)}")
