464f54ff160b54d1d0a59daa475a28051ba459a0
shamaliat01 <carnationskies@gmail.com>
2021-12-12 14:47:25 -0500

    updates

--- 2021/day12.py
+++ 2021/day12.py
@@ -1,7 +1,28 @@
-# make adjacency list ?
+# make adjacency list and use dfs 
 from collections import defaultdict
+
+def getAdjacencyList(connections):
+    adjList = defaultdict(set)
+    for connection in connections:
+        try:
+            adjList[connection[0]].add(connection[1])
+        except KeyError:
+            adjList[connection[0]] = {connection[1]}
+        try:
+            adjList[connection[1]].add(connection[0])
+        except KeyError:
+            adjList[connection[1]] = {connection[0]}
+    return adjList
+
 
 input = open("2021/inputs/ex12.txt")
 connections=[tuple(l.strip().split("-")) for l in input]
-print(connections)
 
+adjList = getAdjacencyList(connections)
+
+for cave in adjList:
+    print([cave][-1])
+    print(f"{adjList[cave]}\n")
+
+p = ['start'][-1]
+print(p)
