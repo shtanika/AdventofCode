9ca90b4090bd3a8b821588d0a6797849e1d3d63a
shamaliat01 <carnationskies@gmail.com>
2021-12-12 21:27:01 -0500

    parth one

--- 2021/day12.py
+++ 2021/day12.py
@@ -14,15 +14,54 @@ def getAdjacencyList(connections):
             adjList[connection[1]] = {connection[0]}
     return adjList
 
+# gets one path
+def dfs(start,visited,adjList):
+    path=[]
+    stack=[]
 
-input = open("2021/inputs/ex12.txt")
+    stack.append('start')
+    visited['start'] = True
+    cave=stack[0]
+    print(start)
+
+    while(cave!="end"):
+        cave = stack.pop()
+        path.append(cave)
+        print(f"{cave} ")
+
+        connections = adjList[cave]
+
+        for i in connections:
+            if(not visited[i]):
+                stack.append(i)
+                if(i.islower()):
+                    visited[i] = True
+    print(path)
+
+def getPaths(node, end, visited, adjList, path, paths):
+    if (node.islower()):
+        visited.add(node)
+    path.append(node)
+
+    if node == end:
+        paths.append(path.copy())
+    else:
+        for nxt in adjList[node]:
+            if nxt not in visited:
+                getPaths(nxt,end,visited,adjList,path,paths)
+    path.pop()
+    if node.islower():
+        visited.remove(node)
+    return paths
+
+# PART ONE
+input = open("2021/inputs/12.txt")
 connections=[tuple(l.strip().split("-")) for l in input]
-
 adjList = getAdjacencyList(connections)
 
-for cave in adjList:
-    print([cave][-1])
-    print(f"{adjList[cave]}\n")
+visited = set()
+paths=[]
+path=[]
+paths = getPaths('start','end',visited,adjList,path,paths)
 
-p = ['start'][-1]
-print(p)
+print(f"Number of paths that visit small caves at most once: {len(paths)}")
